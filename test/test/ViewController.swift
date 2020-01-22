45cd8a2f596651cb6e5b03e6affc8c3278e27a7c
Yi Li <licds@bc.edu>
2020-01-21 19:19:17 -0500

    add test

---
+++ test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Yi Li on 1/21/20.
+//  Copyright © 2020 Yi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

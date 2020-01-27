3cdc93710dc9643566ff16c85efb90a8058e0767
Yi Li <licds@bc.edu>
2020-01-27 18:35:07 -0500

    Renamed photos and added animal name

--- LunarZodiac/LunarZodiac/ViewController.swift
+++ LunarZodiac/LunarZodiac/ViewController.swift
@@ -11,19 +11,23 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var yearImages: UIImageView!
+    @IBOutlet weak var animalNamesLabel: UILabel!
     var startingYear = 2020
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    var animalNames = ["RAT", "OX", "TIGER", "RABBIT", "DRAGON", "SNAKE", "HORSE", "GOAT", "MONKEY", "ROOSTER", "DOG", "PIG"]
     @IBAction func nextYearButtonPressed(_ sender: UIButton) { if sender.tag == 1 {
         startingYear += 1
         yearLabel.text = String(startingYear)
-        yearImages.image = UIImage(named: "image\((startingYear-4)%12)")
+        animalNamesLabel.text = animalNames[(startingYear-4)%12]
+        yearImages.image = UIImage(named: animalNames[(startingYear-4)%12])
     } else if sender.tag == 0 {
         startingYear -= 1
         yearLabel.text = String(startingYear)
-        yearImages.image = UIImage(named: "image\((startingYear-4)%12)")
+        animalNamesLabel.text = animalNames[(startingYear-4)%12]
+        yearImages.image = UIImage(named: String(animalNames[(startingYear-4)%12]))
     }
     }
 
